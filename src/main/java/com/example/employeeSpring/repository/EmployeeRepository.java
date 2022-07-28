543c1cf470f3947837d015e5865bb1ee7c356bb9
vetal12210 <vet2000al@gmail.com>
2022-07-28 21:17:35 +0300

    Add SpringBoot project with React framework

---
+++ src/main/java/com/example/employeeSpring/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.example.employeeSpring.repository;
+
+import com.example.employeeSpring.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
